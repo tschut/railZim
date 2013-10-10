ac2c1b17867be82eb8ea1e907f854bc10a470cf3
tschut <tschut@gmail.com>
2013-10-10 19:29:28 +0200

    Cleanup HelpActivity

--- Android/src/com/spacemangames/gravisphere/ui/HelpActivity.java
+++ Android/src/com/spacemangames/gravisphere/ui/HelpActivity.java
@@ -3,12 +3,12 @@ package com.spacemangames.gravisphere.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
 import com.spacemangames.gravisphere.FreezeGameThreadRunnable;
 import com.spacemangames.gravisphere.GamePrefs_;
@@ -19,63 +19,56 @@ import com.spacemangames.library.SpaceData;
 import com.spacemangames.library.SpaceLevel;
 import com.spacemangames.pal.PALManager;
 
-@EActivity
+@EActivity(R.layout.help_layout)
 public class HelpActivity extends Activity {
     private static final String TAG                    = "HelpActivity";
 
     public static final int     HELP_ACTION_START_GAME = 0;
 
-    private Button              mNextButton;
-    private Button              mPrevButton;
+    @ViewById
+    protected Button            nextButton;
+
+    @ViewById
+    protected Button            prevButton;
 
     @Pref
     protected GamePrefs_        gamePrefs;
+
+    @Click(R.id.prevButton)
+    protected void onClickPrev() {
+        PALManager.getLog().v(TAG, "OnClick PrevButton");
+        if (SpaceData.getInstance().mCurrentLevel.mId == SpaceLevel.ID_HELP1) {
+            SpaceView spaceview = (SpaceView) findViewById(R.id.space);
+            spaceview.mIgnoreFocusChange = true;
+            finish();
+        } else {
+            GameThreadHolder.getThread().loadPrevLevel(true);
+        }
+    }
+
+    @Click(R.id.nextButton)
+    protected void onClickNext() {
+        PALManager.getLog().v(TAG, "OnClick NextButton");
+        if (SpaceData.getInstance().mCurrentLevel.mId == SpaceLevel.ID_HELP4) {
+            GameThreadHolder.getThread().postSyncRunnable(new FreezeGameThreadRunnable());
+            Intent i = new Intent();
+            i.putExtra("action", HELP_ACTION_START_GAME);
+            setResult(Activity.RESULT_OK, i);
+            finish();
+        } else {
+            GameThreadHolder.getThread().loadNextLevel(true);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (savedInstanceState == null) {
-            setContentView(R.layout.help_layout);
-
-            // add button handlers
-            mNextButton = (Button) findViewById(R.id.button_next);
-            mNextButton.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    PALManager.getLog().v(TAG, "OnClick NextButton");
-                    if (SpaceData.getInstance().mCurrentLevel.mId == SpaceLevel.ID_HELP4) {
-                        GameThreadHolder.getThread().postSyncRunnable(new FreezeGameThreadRunnable());
-                        Intent i = new Intent();
-                        i.putExtra("action", HELP_ACTION_START_GAME);
-                        setResult(Activity.RESULT_OK, i);
-                        finish();
-                    } else {
-                        GameThreadHolder.getThread().loadNextLevel(true);
-                    }
-                }
-            });
-
-            mPrevButton = (Button) findViewById(R.id.button_prev);
-            mPrevButton.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    PALManager.getLog().v(TAG, "OnClick PrevButton");
-                    if (SpaceData.getInstance().mCurrentLevel.mId == SpaceLevel.ID_HELP1) {
-                        SpaceView spaceview = (SpaceView) findViewById(R.id.space);
-                        spaceview.mIgnoreFocusChange = true;
-                        finish();
-                    } else {
-                        GameThreadHolder.getThread().loadPrevLevel(true);
-                    }
-                }
-            });
-
-            // set the shared pref that the help has been shown
             gamePrefs.hasSeenHelp().put(true);
         } else {
             // we are being restored: restart the app
-            Intent i = new Intent(this, com.spacemangames.gravisphere.ui.LoadingActivity.class);
+            Intent i = new Intent(this, LoadingActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(i);
             finish();
