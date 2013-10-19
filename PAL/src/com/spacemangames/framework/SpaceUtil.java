46ab746e0480cc0a728a46c464dd4beb05e66c19
tschut <tschut@gmail.com>
2013-10-19 17:24:18 +0200

    Variable renaming

--- PAL/src/com/spacemangames/framework/SpaceUtil.java
+++ PAL/src/com/spacemangames/framework/SpaceUtil.java
@@ -5,79 +5,79 @@ import com.spacemangames.math.Rect;
 import com.spacemangames.math.RectF;
 
 public class SpaceUtil {
-    private static float mScaleForResolution = 1.0f;
+    private static float SCALE_FOR_RESOLUTION = 1.0f;
 
-    private static RectF mScratchRectF1      = new RectF();
-    private static RectF mScratchRectF2      = new RectF();
+    private static RectF SCRATCH_RECTF1      = new RectF();
+    private static RectF SCRATCH_RECTF2      = new RectF();
 
-    public static float  mDPI;
+    public static float  DPI;
 
     public static float  BASELINE_WIDTH      = 800f;
     public static float  BASELINE_HEIGHT     = 480f;
 
     public static void init(float xDpi, float yDpi) {
-        mDPI = Math.min(xDpi, yDpi);
+        DPI = Math.min(xDpi, yDpi);
     }
 
-    public static void setResolution(Rect aRes) {
+    public static void setResolution(Rect resolution) {
         // the baseline is 800x480...
         float scaleX, scaleY;
-        scaleX = aRes.width() / BASELINE_WIDTH;
-        scaleY = aRes.height() / BASELINE_HEIGHT;
-        mScaleForResolution = 1f / Math.min(scaleX, scaleY);
+        scaleX = resolution.width() / BASELINE_WIDTH;
+        scaleY = resolution.height() / BASELINE_HEIGHT;
+        SCALE_FOR_RESOLUTION = 1f / Math.min(scaleX, scaleY);
     }
 
     public static float cmToPixels(float cm) {
         float inch = cm * 0.393700787f;
-        return inch * mDPI;
+        return inch * DPI;
     }
 
     public static float pixelsToCm(float pixels) {
-        return (pixels / mDPI) * (1 / 0.393700787f);
+        return (pixels / DPI) * (1 / 0.393700787f);
     }
 
     public static float resolutionScale(float in) {
-        return in * mScaleForResolution;
+        return in * SCALE_FOR_RESOLUTION;
     }
 
     public static int resolutionScale(int in) {
-        return (int) (in * mScaleForResolution);
+        return (int) (in * SCALE_FOR_RESOLUTION);
     }
 
-    public static float scaleX(Rect in, Rect out, float aX) {
-        return ((float) out.width() / (float) in.width()) * aX;
+    public static float scaleX(Rect in, Rect out, float x) {
+        return ((float) out.width() / (float) in.width()) * x;
     }
 
-    public static float scaleY(Rect in, Rect out, float aY) {
-        return ((float) out.height() / (float) in.height()) * aY;
+    public static float scaleY(Rect in, Rect out, float y) {
+        return ((float) out.height() / (float) in.height()) * y;
     }
 
-    public static float transformX(RectF in, RectF out, float aX) {
-        return (out.width() / in.width()) * (aX - in.left);
+    public static float transformX(RectF in, RectF out, float x) {
+        return (out.width() / in.width()) * (x - in.left);
     }
 
-    public static float transformX(Rect in, Rect out, float aX) {
-        return ((float) out.width() / (float) in.width()) * (aX - in.left);
+    public static float transformX(Rect in, Rect out, float x) {
+        return ((float) out.width() / (float) in.width()) * (x - in.left);
     }
 
-    public static float transformY(RectF in, RectF out, float aY) {
-        return (out.height() / in.height()) * (aY - in.top);
+    public static float transformY(RectF in, RectF out, float y) {
+        return (out.height() / in.height()) * (y - in.top);
     }
 
-    public static float transformY(Rect in, Rect out, float aY) {
-        return ((float) out.height() / (float) in.height()) * (aY - in.top);
+    public static float transformY(Rect in, Rect out, float y) {
+        return ((float) out.height() / (float) in.height()) * (y - in.top);
     }
 
-    public static void transformCoordinates(Rect in, Rect out, Vector2 vec) {
-        mScratchRectF1.set(in);
-        mScratchRectF2.set(out);
-        transformCoordinates(mScratchRectF1, mScratchRectF2, vec);
+    public static void transformCoordinates(Rect in, Rect out, Vector2 vector) {
+        SCRATCH_RECTF1.set(in);
+        SCRATCH_RECTF2.set(out);
+        transformCoordinates(SCRATCH_RECTF1, SCRATCH_RECTF2, vector);
     }
 
-    public static void transformCoordinates(RectF in, RectF out, Vector2 vec) {
-        float lTransformedX = transformX(in, out, vec.x);
-        float lTransformedY = transformY(in, out, vec.y);
-        vec.set(lTransformedX, lTransformedY);
+    public static void transformCoordinates(RectF in, RectF out, Vector2 vector) {
+        float lTransformedX = transformX(in, out, vector.x);
+        float lTransformedY = transformY(in, out, vector.y);
+        vector.set(lTransformedX, lTransformedY);
     }
 
 }
